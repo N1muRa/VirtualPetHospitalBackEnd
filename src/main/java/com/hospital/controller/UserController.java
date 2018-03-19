9a3f5d3872fd67d4718a69431570fa98ea136382
308170166@qq.com <961922961105wc>
2018-03-19 09:01:40 +0800

    增加用户登录验证功能

--- src/main/java/com/hospital/controller/UserController.java
+++ src/main/java/com/hospital/controller/UserController.java
@@ -8,12 +8,18 @@ import com.hospital.service.UserService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hibernate.HibernateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by WangCheng on 2018/3/13.
@@ -27,7 +33,8 @@ public class UserController {
 
     private final Log log = LogFactory.getLog(getClass());
 
-    @RequestMapping(value = "getUser/{page}", method = RequestMethod.GET, produces="application/json")
+//    获得指定页码的用户信息
+    @RequestMapping(value = "admin/user/{page}", method = RequestMethod.GET, produces="application/json;charset=UTF-8")
     @ResponseBody
     public String getUsers(@PathVariable String page){
         int _page = Integer.parseInt(page);
@@ -55,7 +62,8 @@ public class UserController {
         return "{\"data\":"+json+",\"pages\":"+pageCount+"}";
     }
 
-    @RequestMapping(value = "updateUser", method = RequestMethod.POST, produces = "application/json")
+//    更新用户
+    @RequestMapping(value = "admin/user", method = RequestMethod.PUT, produces = "application/json;charset=UTF-8")
     @ResponseBody
     public String updateUser(@RequestBody User user) {
         try {
@@ -66,7 +74,8 @@ public class UserController {
         return "{\"result\":true}";
     }
 
-    @RequestMapping(value = "saveUser", method = RequestMethod.PUT, produces = "application/json")
+//    增加用户
+    @RequestMapping(value = "admin/user", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     @ResponseBody
     public String saveUser(@RequestBody User user) {
         try {
@@ -77,11 +86,43 @@ public class UserController {
         return "{\"result\":true}";
     }
 
-    @RequestMapping(value = "deleteUser/{id}", method = RequestMethod.DELETE, produces = "application/json")
+//    删除用户
+    @RequestMapping(value = "admin/user/{id}", method = RequestMethod.DELETE, produces = "application/json;charset=UTF-8")
     @ResponseBody
     public String deleteUser(@PathVariable String id) {
         int _id = Integer.parseInt(id);
         userService.deleteUser(_id);
         return "{\"result\":true}";
+    }
+
+    @RequestMapping(value = "validate", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public Map<String, String> validate(@RequestBody Map map, HttpSession session) {
+        BufferedReader in= null;
+        String captcha =  "";
+        try {
+            in = new BufferedReader(new FileReader("code.txt"));
+            captcha = in.readLine();
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Map<String, String> result = new HashMap<String, String>();
+        if (map.get("captcha").toString().equalsIgnoreCase(captcha) || captcha.equals("")) {  //忽略验证码大小写
+            User u = userService.getUser(map.get("userName").toString());
+            if (u != null && u.getPassword().equals(map.get("userPwd").toString())) {
+                result.put("isValidated", "true");
+                result.put("userType", u.getUsertype().toString());
+            } else {
+                result.put("isValidated", "false");
+                result.put("err", "填写信息错误");
+            }
+        } else {
+            result.put("isValidated", "false");
+            result.put("err", "填写信息错误");
+        }
+        return result;
     }
 }
