eaea16e9c24d04c12775f17410ab3dc25b9443d9
VamZero <308170166@qq.com>
2018-04-09 20:02:02 +0800

    Add file upload and download module

---
+++ src/main/java/com/hospital/controller/FileController.java
@@ -0,0 +1,63 @@
+package com.hospital.controller;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by WangCheng on 2018/4/1.
+ */
+
+@Controller
+public class FileController {
+
+    /**
+     * 实现文件上传
+     *
+     * @param uploadFile 多文件上传的数组
+     * @return 返回成功与否的页面
+     * @throws Exception
+     */
+    @RequestMapping(value="upload",method = RequestMethod.POST)
+    @ResponseBody
+    public String Upload(@RequestParam(value = "file",required = false) MultipartFile uploadFile, HttpServletRequest request)throws Exception {
+        String fileName = uploadFile.getOriginalFilename();
+//        String leftPath = session.getServletContext().getContextPath("/images");
+//        String classpath = this.getClass().getResource("/").getPath();
+
+        //target/PetHospital/target/PetHospital/WEB-INF/classes/META-INF/files
+
+        String leftPath = "/User/no1r/IdeaProjects/VirtualPetHospitalBackEnd/src/main/webapp/img";//classpath+"META-INF/files";
+        String returnPath = "http://localhost:8888/img/" + fileName;
+        File file = new File(leftPath, fileName);
+        uploadFile.transferTo(file);
+        return "{\"fileName\":\"" + returnPath + "\",\"result\":true}";
+    }
+
+    /**
+     * 实现文件下载
+     * @return 下载实体数据流
+     * @throws IOException
+     */
+    @RequestMapping(value = "download",method = RequestMethod.POST)
+    public ResponseEntity<byte[]> Download()throws IOException{
+        File file = new File("~/Download");
+        HttpHeaders headers = new HttpHeaders();
+        String fileName = new String("你好.jsp".getBytes("UTF-8"),"iso-8859-1");
+        headers.setContentDispositionFormData("attachment",fileName);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),headers, HttpStatus.CREATED);
+    }
+}
